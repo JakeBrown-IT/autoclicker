9d0781a3eff695606a992452507e94cac70d87d9
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-09-25 23:37:04 +0100

    docs: added docstrings to class and all methods.

--- autoclicker.py
+++ autoclicker.py
@@ -11,17 +11,29 @@ import threading
 
 # AutoClicker Class
 class AutoClicker:
+    """
+    A class to envelope all methods to do with the AutoClicker.
+
+    Attributes:
+        click_position (list): The (x, y) position to click at.
+        click_rate (float): The rate of clicks per second.
+        clicking (bool): The state of the autoclicker actively clicking.
+        running (bool): The state of the autoclicker running.
+        click_thread (Thread): The thread for the autoclicker.
+    """
     def __init__(self):
+        """ Initialises the AutoClicker instance. """
         self.click_position = self.set_click_position()
         self.click_rate = pyautogui.PAUSE = 0.1
         self.clicking = False
         self.running = True
-        self.click_thread = None    # Initialise thread to None
+        self.click_thread = None    # Set thread to None while not configured.
 
         keyboard.add_hotkey('ctrl+shift+f', self.toggle_clicking)
         #keyboard.add_hotkey('ctrl+shift+p', self.set_click_position)
 
     def autoclick(self):
+        """ Automatically clicks the set position using the click_rate. """
         while self.running:
             if self.clicking:
                 pyautogui.click(self.click_position)
@@ -30,6 +42,7 @@ class AutoClicker:
                 time.sleep(0.1)
 
     def toggle_clicking(self):
+        """ Toggle the AutoClicker for performing other tasks. """
         if not self.clicking:
             self.clicking = True
 
@@ -45,12 +58,14 @@ class AutoClicker:
             print("AutoClicker stopped.")
 
     def set_click_position(self):
+        """ Set the position for the AutoClicker to click. """
         print("Move mouse into position and press Enter.")
         keyboard.wait('enter')
         self.click_position = pyautogui.position()
         print(f"Click position set to: {self.click_position}")
 
-    def kill(self):        
+    def kill(self):
+        """ Kill the AutoClicker thread. """        
         self.running = False
 
         if self.click_thread is not None:
@@ -58,9 +73,8 @@ class AutoClicker:
         
         print("AutoClicker killed.")
 
-
-
     def run(self):
+        """ Run the autoclicker until killed. """
         print("Press '<C-F>' to toggle the AutoClicker on/off.")
         #print("Press '<C-P>' to set the click position.")
         print("Press '<C-c>' to exit.")
@@ -76,51 +90,3 @@ class AutoClicker:
 if __name__ == "__main__":
     autoclicker = AutoClicker()
     autoclicker.run()
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-# set click interval to 0.1 second (10 clicks per second)
-interval = 0.1
-pyautogui.PAUSE = interval 
-
-def autoclick(x, y):
-    clicks = 0
-    start = time.time()
-
-    while not keyboard.is_pressed('q'):
-        pyautogui.moveTo(x, y)
-        pyautogui.click()
-        clicks += 1
-
-    end = time.time()
-    duration = end - start
-
-    print(f"{clicks} clicks in {duration:.3f} seconds."
-          .format(clicks, duration))
-
-    return
-
-def main():
-    while True:
-        if keyboard.is_pressed('l'):
-            x, y = pyautogui.position()
-            autoclick(x, y)
-            break
-
-    return
-
-
-if __name__ == "__main__":
-    main()
