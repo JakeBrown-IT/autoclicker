6cacd99695cd66ebd6d93f75febea499810241d9
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-09-25 21:54:32 +0100

    feat: added duration and click count to output

--- autoclicker.py
+++ autoclicker.py
@@ -3,41 +3,36 @@
 # Created: 25-09-2024
 # Purpose: An autoclicker program using 'pyautogui'
 
-# the loop will start when the user enters the l key
-# at this point, the program captures the position of the cursor
-# it locks this position in, and 
-# the loop will autoclick at 10 clicks per second
-# 
-
 # imports
 import pyautogui
 import keyboard
+import time
 
 # set click interval to 0.1 second (10 clicks per second)
 interval = 0.1
 pyautogui.PAUSE = interval 
 
 def autoclick(x, y):
-    print(f"[log] locked mouse position at ({x}, {y})...".format(x, y))
-    print(f"[log] starting autoclicks at {interval}s...".format(interval))
+    clicks = 0
+    start = time.time()
 
     while not keyboard.is_pressed('q'):
         pyautogui.moveTo(x, y)
         pyautogui.click()
+        clicks += 1
 
-    # print 
-    print("[log] 'q' pressed, quitting...")
+    end = time.time()
+    duration = end - start
 
-    exit()
+    print(f"{clicks} clicks in {duration} seconds."
+          .format(duration, clicks))
+
+    return
 
 def main():
-    print("[log] autoclicker launched...")
-    print("[log] waiting for 'l' keypress...")
-    
     while True:
         if keyboard.is_pressed('l'):
             x, y = pyautogui.position()
-
             autoclick(x, y)
             break
 
